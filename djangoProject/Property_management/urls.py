c961bb3ce6ffc218d501f4eec36d58d234a3c31f
Amanda <141260537+amandaofficial@users.noreply.github.com>
2024-10-23 04:38:34 -0700

    urls.py

--- djangoProject/Property_management/urls.py
+++ djangoProject/Property_management/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#after running server in site we enter /admin and then you login to your site.
+
 from django.contrib import admin
 from django.urls import path
 
