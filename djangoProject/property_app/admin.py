ed7247f9db081da8092b498f3c61f5ad4db31e9e
IGIRIMPUHWE Jonathan <155901195+igirimpuhw@users.noreply.github.com>
2024-10-23 13:34:13 +0200

    admin.py

--- djangoProject/property_app/admin.py
+++ djangoProject/property_app/admin.py
@@ -1,3 +1,4 @@
+#in admin site super user is allow to add property, tenant, unit and lease. and he or she can change credentails of any user.
 from django.contrib import admin
 
 from property_app.models import Property,Tenant,Unit,Lease
